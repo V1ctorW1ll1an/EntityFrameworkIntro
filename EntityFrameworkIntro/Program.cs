2704afed53bebcad3c79f06f7d6ddbfe48bccd44
Victor Willian <victorwillian1012@gmail.com>
2023-06-21 17:23:40 -0300

    chore: :white_check_mark: Add context, models and factory

--- EntityFrameworkIntro/Program.cs
+++ EntityFrameworkIntro/Program.cs
@@ -1,3 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
 Console.WriteLine("Hello, World!");
 
+#region Models
+class Dish
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string? Notes { get; set; }
+
+    public int? Stars { get; set; }
+
+    public List<DishIngredients>? Ingredients { get; set; } = new();
+}
+
+class DishIngredients
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+
+    [MaxLength(50)]
+    public string UniOfMeasure { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(5,2)")]
+    public decimal Amount { get; set; }
+
+    public Dish? Dish { get; set; }
+
+    public int DishId { get; set; }
+}
+
+#endregion
+
+#region Context
+class CookBookContext : DbContext
+{
+    public DbSet<Dish> Dishes { get; set; }
+
+    public DbSet<DishIngredients> Ingredients { get; set; }
+
+    public CookBookContext(DbContextOptions<CookBookContext> options)
+        : base(options) { }
+}
+
+#endregion
+
+#region Factory
+class CookBookContextFactory : IDesignTimeDbContextFactory<CookBookContext>
+{
+    public CookBookContext CreateDbContext(string[] args)
+    {
+        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<CookBookContext>();
+        // postgresql
+        optionsBuilder.UseNpgsql(builder.GetConnectionString("DefaultConnection"));
+
+        return new CookBookContext(optionsBuilder.Options);
+    }
+}
+#endregion
